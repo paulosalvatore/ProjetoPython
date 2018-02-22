3dffc0a21707cbe192e170c4a94dd62e8701019d
Paulo Salvatore <salvatoregames@gmail.com>
2018-02-22 13:00:59 -0300

    Testes com gráficos 'matplotlib'

---
+++ graficos.py
@@ -0,0 +1,129 @@
+"""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.title("Teste")
+plt.plot([0, 10, 20, 30])
+plt.show()
+"""
+
+"""
+import matplotlib.pyplot as plt
+
+# Pie chart, where the slices will be ordered and plotted counter-clockwise:
+labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+sizes = [15, 30, 45, 10]
+explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+fig1, ax1 = plt.subplots()
+ax1.pie(
+	sizes,
+	explode=explode,
+	labels=labels,
+	autopct='%1.1f%%',
+	shadow=True,
+	startangle=90
+)
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+plt.show()
+"""
+
+"""
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import numpy as np
+
+fig = plt.figure()
+ax = fig.gca(projection="3d")
+
+# Make data.
+X = np.arange(-5, 5, 0.25)
+Y = np.arange(-5, 5, 0.25)
+X, Y = np.meshgrid(X, Y)
+R = np.sqrt(X ** 2 + Y ** 2)
+Z = np.sin(R)
+
+# Plot the surface.
+surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+
+# Customize the z axis.
+ax.set_zlim(-1.01, 1.01)
+ax.zaxis.set_major_locator(LinearLocator(10))
+ax.zaxis.set_major_formatter(FormatStrFormatter("%.02f"))
+
+ax.set_xlabel("Eixo X")
+ax.set_ylabel("Eixo Y")
+ax.set_zlabel("Eixo Z")
+
+# Add a color bar which maps values to colors.
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
+plt.show()
+"""
+
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+data = [[ 66386, 174296,  75131, 577908,  32015],
+        [ 58230, 381139,  78045,  99308, 160454],
+        [ 89135,  80552, 152558, 497981, 603535],
+        [ 78415,  81858, 150656, 193263,  69638],
+        [139361, 331509, 343164, 781380,  52269]]
+
+columns = ('Freeze', 'Wind', 'Flood', 'Quake', 'Hail')
+rows = ['%d year' % x for x in (100, 50, 20, 10, 5)]
+
+values = np.arange(0, 2500, 500)
+value_increment = 1000
+
+# Get some pastel shades for the colors
+colors = plt.cm.BuPu(np.linspace(0, 0.5, len(rows)))
+n_rows = len(data)
+
+index = np.arange(len(columns)) + 0.3
+bar_width = 0.4
+
+# Initialize the vertical-offset for the stacked bar chart.
+y_offset = np.zeros(len(columns))
+
+# Plot bars and create text labels for the table
+cell_text = []
+for row in range(n_rows):
+    plt.bar(index, data[row], bar_width, bottom=y_offset, color=colors[row])
+    y_offset = y_offset + data[row]
+    cell_text.append(['%1.1f' % (x / 1000.0) for x in y_offset])
+# Reverse colors and text labels to display the last value at the top.
+colors = colors[::-1]
+cell_text.reverse()
+
+# Add a table at the bottom of the axes
+the_table = plt.table(cellText=cell_text,
+                      rowLabels=rows,
+                      rowColours=colors,
+                      colLabels=columns,
+                      loc='bottom')
+
+# Adjust layout to make room for the table:
+plt.subplots_adjust(left=0.2, bottom=0.2)
+
+plt.ylabel("Loss in ${0}'s".format(value_increment))
+plt.yticks(values * value_increment, ['%d' % val for val in values])
+plt.xticks([])
+plt.title('Loss by Disaster')
+
+plt.show()
+"""
+
+"""
+import matplotlib.pyplot as plt
+
+plt.plot([10, 20, 30, 45], [15, 40, 75, 80], linestyle='--', color='r', marker='s', linewidth=3.0)
+plt.axis([0, 50, 0, 90])
+plt.show()
+"""
